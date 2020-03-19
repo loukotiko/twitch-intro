764f6b608dc6763545a565017eaa7015568fd79e
Louko&Tiko <38463142+loukotiko@users.noreply.github.com>
2020-03-19 18:39:56 +0100

    Add files via upload

--- src/Plane.jsx
+++ src/Plane.jsx
@@ -1,6 +1,6 @@
 define(function(require) {
   const $smokes = document.getElementById("smoke_layer");
-  const $plane = document.getElementById("plane");
+  const $planeLayer = document.getElementById("plane_layer");
   const $planeSound = document.getElementById("plane_sound");
 
   function createSmoke() {
@@ -9,7 +9,7 @@ define(function(require) {
     $smoke.className = "smoke";
     $smoke.style.backgroundImage = `url(./assets/fumee_${variation}.png)`;
     $smoke.style.top =
-      Math.random() * 10 - 5 + $plane.getBoundingClientRect().top + "px";
+      Math.random() * 10 - 5 + $planeLayer.getBoundingClientRect().top + "px";
     $smokes.appendChild($smoke);
     setTimeout(() => $smokes.removeChild($smoke), 600);
   }
